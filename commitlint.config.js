09dfc261d8af083d1ffe0cad0f29124229d712d5
Nicholas Gates <nickcgates@gmail.com>
2021-02-10 16:30:45 -0500

    feat: QPPA-4772 improving the commit message experience

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,14 @@
 // commitlint.config.js
 module.exports = {
-  plugins: [],
-    extends: ["@commitlint/config-conventional"],
-  
+  plugins: ["commitlint-plugin-jira-rules"],
+  extends: ["@commitlint/config-conventional", "jira"],
+  rules: {
+    // https://commitlint.js.org/#/reference-rules
+    "header-min-length": [2, "always", 12],
+    "subject-case": [
+      2,
+      "always",
+      ["sentence-case", "start-case", "pascal-case", "upper-case"],
+    ],
+  },
 };
